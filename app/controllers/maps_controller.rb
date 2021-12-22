ac2c171522a8f6c91de1d8e22d3827c11bea98fd
patty <pattycheng2008@gmail.com>
2021-12-22 23:25:50 +0800

    update API key

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,4 +1,4 @@
 class MapsController < ApplicationController
-    def index
-    end
+  def index
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
-    def index
-    end
+  def index
+  end
 
     
 end
