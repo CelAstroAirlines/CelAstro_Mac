33794d4b7b4b12bc076d97a4898c7845ab9b3c62
Derek <chungmin18@gmail.com>
2021-12-24 22:09:42 +0800

    modified schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_23_234951) do
+ActiveRecord::Schema.define(version: 2021_12_24_063744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -97,12 +97,11 @@ ActiveRecord::Schema.define(version: 2021_12_23_234951) do
     t.integer "ticket_amount"
     t.string "departure"
     t.string "arrival"
-    t.string "airport"
-    t.datetime "date"
     t.string "seat_no"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "ticket_id"
+    t.string "departure_date"
   end
 
   create_table "users", force: :cascade do |t|
