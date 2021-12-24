1ac6802c46b17e83a9d4e6865bda223d1552f208
Cheng-Kai-Ming <k_sky369@yahoo.com.tw>
2021-12-24 08:25:23 +0800

    rename seat

--- db/migrate/20211223234951_add_column.rb
+++ db/migrate/20211223234951_add_column.rb
@@ -2,7 +2,7 @@ class AddColumn < ActiveRecord::Migration[6.1]
   def change
     change_table :seats do |t|
       50.times do |i|
-        t.boolean :"seatNo#{i}"
+        t.boolean :"seat_no#{i}"
       end
     end
   end
