981bd72efb7357baed4435abb4b5363e27a87ad0
Cheng-Kai-Ming <k_sky369@yahoo.com.tw>
2021-12-24 08:00:50 +0800

    generate seats model

--- app/frontend/scripts/seat.js
+++ app/frontend/scripts/seat.js
@@ -1,9 +1,39 @@
+import axios from "axios"
+
 document.addEventListener('turbolinks:load', () => {
-  const seats = document.querySelector("#seats")
-  if (seats) {
-    seats.addEventListener("click", (e) => {
-      console.log(e.target.id)
+  const seats = document.querySelectora('input[type="checkbox"]:checked')
+  const csrf_token = document.querySelector("meta[name='csrf-token']")
+  const seat_confirm = document.querySelector("#seat_confirm")
+
+
+
+
+  if (seat_confirm) {
+    seat_confirm.addEventListener("click", () => {
+      console.log(seats)
     })
   }
+
+
+
+  seats.addEventListener("click", (res) => {
+    if (res.target.checked === true) {
+      return res.target.id
+    }
+  })
+
+
+  // if (seat_confirm) {
+  //   seat_confirm.addEventListener("click", () => {
+  //     axios.defaults.headers.common['X-CSRF-TOKEN'] = csrf_token.content
+  //     axios.post("/api/v1/tickets/1/seats").then((res) => {
+  //       console.log(res.data)
+  //     })
+  //   })
+  // }
+
+
+
+
 
 })
