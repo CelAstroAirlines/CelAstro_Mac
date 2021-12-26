acf843442eea571142d7a4325543be86bb07cfe5
Cheng-Kai-Ming <k_sky369@yahoo.com.tw>
2021-12-26 12:03:57 +0800

    pick choosed seat will fail

--- app/frontend/scripts/book_seat.js
+++ app/frontend/scripts/book_seat.js
@@ -7,29 +7,48 @@ document.addEventListener('turbolinks:load', () => {
   const seats = document.querySelector('#seats')
   let formated_seat = {}
 
-  seats.addEventListener("click", (res) => {
-    if (res.target.checked === true) {
-      formated_seat[`${res.target.id}`] = true
+  function choose_seats(click_event) {
+    if (click_event.target.checked === true) {
+      formated_seat[`${click_event.target.id}`] = true
       // console.log(res.target.id)
       // console.log(formated_seat)
     } else {
-      delete formated_seat[`${res.target.id}`]
+      delete formated_seat[`${click_event.target.id}`]
     }
+  }
+
+  async function api_seats_check(click_event) {
+    let api_seat_check = {}
+    api_seat_check["seat"] = click_event.target.id
+    axios.defaults.headers.common['X-CSRF-TOKEN'] = csrf_token.content
+    let res = await axios.post(`/api/v1/tickets/1/seats/check`, api_seat_check);
+    let data = res.data.result;
+    console.log(data)
+    if (data === "false") {
+      alert('此位已被選取');
+    } else {
+      choose_seats(click_event)
+    };
+  }
+
+
+  seats.addEventListener("click", (res) => {
+    // choose_seats(res)
+    api_seats_check(res)
   })
 
-  async function makeGetRequest(all_seats) {
+  async function api_seat_confirm(all_seats) {
     axios.defaults.headers.common['X-CSRF-TOKEN'] = csrf_token.content
     let seats_key = {}
     seats_key["seat"] = all_seats
     let res = await axios.post(`/api/v1/tickets/1/seats/update`, seats_key);
     let data = res.data;
-    console.log(data);
   }
 
   function confirm_seats() {
     if (seat_confirm) {
       seat_confirm.addEventListener("click", () => {
-        makeGetRequest(formated_seat);
+        api_seat_confirm(formated_seat);
       })
     }
   }
--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,6 @@ Rails.application.routes.draw do
     end
   end
   post '/api/v1/tickets/:id/seats/update' , to: 'api/v1/seats#update_seat'
+  post '/api/v1/tickets/:id/seats/check', to: 'api/v1/seats#check'
 
 end
