52e2d84ed5cf062a2829d1251ca34adfb24da1de
Iris Kuo <iriskuo0523@gmail.com>
2021-12-28 17:30:57 +0800

    新增after sign in view & 更新sign in view logo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
+  private
+  def after_sign_in_path_for(resource)
+     member_path(current_user)
+  end
 end
