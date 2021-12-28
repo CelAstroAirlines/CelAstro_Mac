d344243fef69f03eb91cc1c28fb00e80c39b0373
Iris Kuo <iriskuo0523@gmail.com>
2021-12-28 18:48:26 +0800

    新增after sign in view && 更新sign in view logo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/pbadmin', as: 'rails_admin'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
@@ -9,14 +8,10 @@ Rails.application.routes.draw do
   get "/payment", to: "orders#payment"
   # post "/receipt",  to: "orders#receipt"
 
-  # ticket
-  resources :tickets
-
-  #search
+  # google map
+  resources :maps, only: [:index]
+  resources :tickets, only: [:index]
   resources :searches
-
-  #cart
-  resources :carts
 
 
   namespace :api do
