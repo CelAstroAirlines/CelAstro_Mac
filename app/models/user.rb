59f61af5ef39201dc8e777aeea5b495fa9e437d5
ellywu <lian1115.lw@gmail.com>
2021-12-30 16:52:59 +0800

    adjusting spacing for homepage

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,  :trackable 
 
+
+
+
   has_many :tickets
   has_one :cart
 end
