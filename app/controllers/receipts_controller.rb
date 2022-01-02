cd910d6ccf7d1399622c8d014c092fa97e8a5f1e
Iris Kuo <iriskuo0523@gmail.com>
2022-01-02 13:17:17 +0800

    新增 edit view

--- app/controllers/receipts_controller.rb
+++ app/controllers/receipts_controller.rb
@@ -1,15 +1,7 @@
 class ReceiptsController < ApplicationController
-  skip_before_action :verify_authenticity_token, :only => [:create]
+  skip_before_action :verify_authenticity_token, :only => [:receivempg]
 
-  # def payment
-  #   @form_info = Newebpay::Mpg.new.form_info
-  # end
-
-  def create
-    render html:"123"
-
+  def receivempg
+    render html:params
   end
-
-  
-
 end
