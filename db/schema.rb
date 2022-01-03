3636c5bfba81307bcaf4be3adb64ac6595a51036
Derek <chungmin18@gmail.com>
2022-01-03 14:52:17 +0800

    add quantity to ticket

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_01_103733) do
+ActiveRecord::Schema.define(version: 2022_01_03_063839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -116,6 +116,7 @@ ActiveRecord::Schema.define(version: 2022_01_01_103733) do
     t.string "return_arrival_airport"
     t.string "return_departure_time"
     t.string "return_arrival_time"
+    t.integer "quantity"
   end
 
   create_table "users", force: :cascade do |t|
