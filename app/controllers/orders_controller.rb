3ff55aaa7e4fc2c3275614e4fb00fe781742bde6
Iris Kuo <iriskuo0523@gmail.com>
2022-01-04 19:27:49 +0800

    新增cart total price to newebpay & 修改藍新回傳頁面

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,24 +1,15 @@
 class OrdersController < ApplicationController
   skip_before_action :verify_authenticity_token, :only => [:receivempg]
   def payment
-    # p current_cart.total_price
     @form_info = Newebpay::Mpg.new(current_cart.total_price).form_info
   end
 
   def receivempg
-    # render html:params
     @response = Newebpay::MpgResponse.new(params[:TradeInfo])
     if @response.status === "SUCCESS"
        flash.now[:notice] = "付款成功！"
     else
        redirect_to cart_path, notice: '付款過程發生問題'
     end   
-  end
-
-  private
-  def order_params
-    params.require(:cart)
-          .permit(:tickets)
-          .merge(user: current_user)
   end
 end
