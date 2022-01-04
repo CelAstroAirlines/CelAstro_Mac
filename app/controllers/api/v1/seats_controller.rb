22ad9bb69bd67a2b4363bc57004b3c0088c80755
Cheng-Kai-Ming <k_sky369@yahoo.com.tw>
2022-01-04 15:10:37 +0800

    delete confused code in seat part

--- app/controllers/api/v1/seats_controller.rb
+++ app/controllers/api/v1/seats_controller.rb
@@ -9,17 +9,11 @@ class Api::V1::SeatsController < ApplicationController
       
   end
 
-  
   def index
-    @seat = Seat.where(:plane_id =>params[:id]).order(:id) #給予tickets空位填入班機id
+    @seat = Seat.where(:plane_id =>params[:id]).order(:id)
   end
 
-  def update_seat
-    render html:params
-    # render html:params[:id] #這會抓到機號
-    # render json:{result: params}
-    # render html:params[:seat] #{"seat_no0"=>"true", "seat_no1"=>"true"}
-    
+  def update_seat  
   end
 
   def check    
@@ -28,24 +22,18 @@ class Api::V1::SeatsController < ApplicationController
       :seat_id=> params[:seat][:seat_id], 
       :area => params[:seat][:area]
     )
-
     if @seat.state == "vaccant" 
       @seat.occupied!
-      @seat.update(user_id: current_user)
+      # @seat.update(user_id: current_user)
     elsif @seat.state == "occupied" 
       @seat.vaccant!
-      @seat.update(user_id: nil)      
+      # @seat.update(user_id: nil)      
     end
-    
-
 
     ActionCable.server.broadcast "seats_room_channel_#{params[:plane_id]}", {
       seat_params: @seat,
       message: render(partial: 'seat', locals:{f:@seat})
     }
-
   end
-
-
 
 end
