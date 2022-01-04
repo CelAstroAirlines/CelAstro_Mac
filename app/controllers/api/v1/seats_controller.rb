c1ddf21f53a039db0f79ad9424272379e1208ea1
Cheng-Kai-Ming <k_sky369@yahoo.com.tw>
2022-01-04 22:31:40 +0800

    seats using actioncable aasm and stimulus

--- app/controllers/api/v1/seats_controller.rb
+++ app/controllers/api/v1/seats_controller.rb
@@ -1,22 +1,43 @@
 class Api::V1::SeatsController < ApplicationController
-  
+  # before_action :authenticate_user!
   def create
-    @seat = Seat.new #給予tickets空位填入班機id
+    ticket_id = Seat.find_by(ticket_id: params[:id])
+    if ticket_id == 'true'
+      redirect_to action: "show", id: params[:id]
+    else
+    end
+      
+  end
+
+  
+  def index
+    @seat = Seat.where(:plane_id =>params[:id]).order(:id) 
   end
 
   def update_seat
-    # render html:params[:id] #這會抓到機號
-    # render json:{result: params}
-    # render html:params[:seat] #{"seat_no0"=>"true", "seat_no1"=>"true"}
-    render html:params[:seat]
+
   end
 
-  def check
-    if params[:seat] == "cbox2" 
-      render json: {result: 'false'}
+  def check    
+    @seat = Seat.find_by(
+      plane_id: params[:id], 
+      seat_id: params[:seat][:seat_id], 
+      area: params[:seat][:area]
+    )
+
+    if @seat.state == "vaccant" 
+      @seat.occupied!
+      # @seat.update(user_id: current_user)
+    elsif @seat.state == "occupied" 
+      @seat.vaccant!
+      # @seat.update(user_id: nil)      
     end
+    
+    ActionCable.server.broadcast "seats_room_channel_#{params[:plane_id]}", {
+      seat_params: @seat,
+      message: render(partial: 'seat', locals:{f:@seat})
+    }
+
   end
-
-
 
 end
