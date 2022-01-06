0e874f4f11b75e2e215234e566c8d22fdff83bfd
Iris Kuo <iriskuo0523@gmail.com>
2022-01-06 17:46:37 +0800

    wip

---
+++ app/frontend/controllers/payment_controller.js
@@ -0,0 +1,13 @@
+
+import { Controller } from "stimulus";
+export default class extends Controller {
+
+  static targets = []
+
+  connect(){
+    setTimeout(() => {
+      this.element.submit()
+    }, 1000);
+    
+  }
+}
