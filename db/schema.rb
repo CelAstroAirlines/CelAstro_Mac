3b9844e6a5dac4f7d0f96e2044e7712c91d581a7
Derek <chungmin18@gmail.com>
2022-01-06 17:46:56 +0800

    edited seed, added cabin_class

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2022_01_07_040933) do
+=======
+ActiveRecord::Schema.define(version: 2022_01_06_071827) do
+>>>>>>> edited seed, added cabin_class
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
