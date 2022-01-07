054155bb24fccdf86fb0122b97bf50973dc948b2
Cheng-Kai-Ming <k_sky369@yahoo.com.tw>
2022-01-07 12:24:10 +0800

    rename seat state name

--- app/models/seat.rb
+++ app/models/seat.rb
@@ -1,20 +1,45 @@
 class Seat < ApplicationRecord
   include AASM
+  after_save_commit :lock
+  # def lock(action)
+  #   Seat.transaction do
+  #     lock!
+  #     send.(#{action}!)
+  #     save!
+  #   end
+  # end
   
   aasm column: 'state' do
     state :vaccant, initial: true
     state :occupied, :booked
 
     event :vaccant do
-      transitions from: [:occupied, :booked], to: :vaccant
+      transitions from: :occupy, to: :vaccant
     end
 
-    event :occupied do
-      transitions from: :vaccant, to: :occupied
+    event :occupy do
+      transitions from: :vaccant, to: :occupy
     end
 
-    event :booked do
-      transitions from: :occupied, to: :booked
+    event :book do
+      transitions from: :occupy, to: :book
     end
   end
+
+  def lock
+    if seat.occupy?
+      Seat.transaction do
+        seat.lock!
+        seat.user_id = current_user
+        seat.save!
+      end
+    elsif seat.book?
+      Seat.transaction do
+        seat.lock!
+        seat.user_id = current_user
+        seat.save!
+      end
+    end
+  end
+
 end
