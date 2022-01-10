c1909ee3804007e71667be38868849c49ca47ca7
Iris Kuo <iriskuo0523@gmail.com>
2022-01-10 16:57:54 +0800

    ticket index view

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,10 +1,6 @@
 class Ticket < ApplicationRecord
   has_many :cart_items
-<<<<<<< HEAD
   belongs_to :user
-=======
- 
->>>>>>> dev
   
   validates :departure, presence: true
   validates :arrival, presence: true 
