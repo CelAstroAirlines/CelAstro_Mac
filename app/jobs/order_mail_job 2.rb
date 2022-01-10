8e07e7806dad7738ced798c40c3ca399f755ac0b
Cheng-Kai-Ming <k_sky369@yahoo.com.tw>
2022-01-10 12:32:36 +0800

    delete mailer 2

--- app/jobs/order_mail_job 2.rb
+++
@@ -1,7 +0,0 @@
-class OrderMailJob < ApplicationJob
-  queue_as :default
-
-  def perform(*args)
-    OrderMailer.notify_order('#{current_user.email}').deliver
-  end
-end
--- test/mailers/previews/order_mailer_preview 2.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/order_mailer
-class OrderMailerPreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/order_mailer/notify_order
-  def notify_order
-    OrderMailer.notify_order
-  end
-
-end
