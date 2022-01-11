638d4d17e1be7a82116a70fe036a1bb890072c9a
Derek <chungmin18@gmail.com>
2022-01-11 11:37:34 +0800

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_09_063459) do
+ActiveRecord::Schema.define(version: 2022_01_10_152322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2022_01_09_063459) do
     t.string "return_departure_time"
     t.string "cabin_class"
     t.integer "order_ticket_count"
+    t.string "serial"
     t.index ["ticket_id"], name: "index_orders_on_ticket_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
