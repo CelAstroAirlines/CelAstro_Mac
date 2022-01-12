c58c123209634411657187a33ecaa5111a227b2f
patty <pattycheng2008@gmail.com>
2022-01-12 09:39:46 +0800

    comment ticket validates

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,11 +1,10 @@
 class Ticket < ApplicationRecord
   has_many :cart_items
   has_many :seats
-  belongs_to :user
-
   
-  validates :departure, presence: true
-  validates :arrival, presence: true 
-  validates :departure_date, presence: true
+  # belongs_to :user
+  # validates :departure, presence: true
+  # validates :arrival, presence: true 
+  # validates :departure_date, presence: true
   
 end
