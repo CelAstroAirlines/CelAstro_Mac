b33cdfe84ecfe8a1d85343be5c1638566b00e4ba
Cheng-Kai-Ming <k_sky369@yahoo.com.tw>
2022-01-15 23:48:44 +0800

    fix conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_14_050124) do
+ActiveRecord::Schema.define(version: 2022_01_15_122107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2022_01_14_050124) do
     t.integer "sellign_amount"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "state"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
