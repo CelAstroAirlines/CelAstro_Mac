13426f559580b928ed744b2ffe0be9032d595df1
Derek <chungmin18@gmail.com>
2022-01-16 18:55:10 +0800

    minor editing

--- app/jobs/order_mail_job.rb
+++ app/jobs/order_mail_job.rb
@@ -2,6 +2,6 @@ class OrderMailJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    OrderMailer.notify_order('#{current_user.email}').deliver
+    OrderMailer.notify_order('#{@user.email}').deliver
   end
 end
