2b17f5f61216356912aa3abe16e52c740ad09760
Iris Kuo <iriskuo0523@gmail.com>
2022-01-17 01:16:25 +0800

    修改各國簡介modal

--- app/frontend/scripts/england.js
+++ app/frontend/scripts/england.js
@@ -1,17 +1,17 @@
-document.addEventListener("DOMContentLoaded", function (){
+document.addEventListener("DOMContentLoaded", () => {
  var modal = document.getElementById("Modal");
  var img_ENGLAND = document.getElementById("img_ENGLAND");
  var close_btn = document.getElementsByClassName("close_btn")[0];
  
- img_ENGLAND.addEventListener('click', function (){
+ img_ENGLAND.addEventListener('click',() => {
   modal.style.display = "block";
  });
 
- close_btn.addEventListener('click', function (){
+ close_btn.addEventListener('click',() => {
     modal.style.display = "none";
   });
 
- window.addEventListener('click', function (event){
+ window.addEventListener('click',(event) => {
   if (event.target == modal) {
     modal.style.display = "none";
   }
