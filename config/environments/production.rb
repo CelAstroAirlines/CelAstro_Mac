875a880283c1d4fc9d43b6d81932367da785f631
patty <pattycheng2008@gmail.com>
2022-01-17 18:36:18 +0800

    update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,8 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.assets.compile = true
+  
   # Code is not reloaded between requests.
   config.cache_classes = true
 
