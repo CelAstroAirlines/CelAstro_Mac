e22cafa4a45213e173afa51bbf3c4b7f8328fda7
Cheng-Kai-Ming <k_sky369@yahoo.com.tw>
2022-01-18 22:08:25 +0800

    seat js controller seperate

--- app/frontend/scripts/thailand.js
+++ app/frontend/scripts/thailand.js
@@ -2,20 +2,20 @@ document.addEventListener("DOMContentLoaded", () => {
   const modal2 = document.getElementById("Modal2");
   const img_THAILAND = document.getElementById("img_THAILAND");
   const close_btn2 = document.getElementsByClassName("close_btn2")[0];
-  
-  if(img_THAILAND){
-  img_THAILAND.addEventListener('click', () => {
-   modal2.style.display = "block";
-  });
- 
-  close_btn2.addEventListener('click', () => {
-     modal2.style.display = "none";
-   });
- 
-  window.addEventListener('click', (event) => {
-   if (event.target == modal2) {
-     modal2.style.display = "none";
-   }
-  });
+
+  if (img_THAILAND) {
+    img_THAILAND.addEventListener('click', () => {
+      modal2.style.display = "block";
+    });
+
+    close_btn2.addEventListener('click', () => {
+      modal2.style.display = "none";
+    });
+
+    window.addEventListener('click', (event) => {
+      if (event.target == modal2) {
+        modal2.style.display = "none";
+      }
+    });
   }
- });
+});
