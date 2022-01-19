9c10ae500db43fcedde82f65f1dbfe2903c0ac44
patty <pattycheng2008@gmail.com>
2022-01-19 12:49:06 +0800

    remove seat_state_job.rb

--- app/jobs/seat_state_job.rb
+++
@@ -1,7 +0,0 @@
-require 'sidekiq-scheduler'
-class SeatStateJob < ApplicationJob
-  def perform
-    Seat.where('updated_at <=?', 10.minutes.ago)
-      .each { |seat| seat.empty! if seat.state == 'occupied' }
-  end
-end
