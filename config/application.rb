e3c255f79d64890e68762b3276a7fa35fdea684e
Iris Kuo <iriskuo0523@gmail.com>
2022-01-21 17:50:25 +0800

    新增devise語系切換

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ module CelAstro
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.i18n.fallbacks = [:en]
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -16,5 +17,15 @@ module CelAstro
     #
     config.time_zone = "Taipei"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # 設定載入路徑
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
+
+    # 設定預設語系
+    config.i18n.default_locale = 'zh-TW'
+    I18n.available_locales = [:'zh-TW', :zh, :en]
+
+    # 若缺少該語系則以哪個語系取代
+    config.i18n.fallbacks = {:en => :'zh-TW' }
   end
 end
